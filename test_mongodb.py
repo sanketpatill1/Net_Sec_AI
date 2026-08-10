2a275c86d2c99acd3fa8875eaf11d88e23156f1d
Sanket Patil <patilsanket.tech@gmail.com>
2026-08-10 08:41:30 +0530

    ETL Pipeline Completed

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,12 +1,13 @@
-
 from pymongo.mongo_client import MongoClient
+from urllib.parse import quote_plus
 
-uri = "mongodb+srv://krishaitechnologies:<@password>@cluster0.wnqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+username = "patilsankettech_db_user"
+password = quote_plus("Sanket123")
 
-# Create a new client and connect to the server
+uri = f"mongodb+srv://{username}:{password}@clusternetworksecurity.rlqjlll.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 client = MongoClient(uri)
 
-# Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
